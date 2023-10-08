5050fd885968c199f18bd8518ddb2bd7628252c8
sanjuniperodee <87779571856b@gmail.com>
2023-10-08 15:45:42 +0600

    new

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -109,7 +109,7 @@ function Login() {
             setValidate(code)
             console.log(validate + " " + code)
             let data = phoneNumber.replaceAll(/[^0-9]/g, '')
-            // await fetch("https://api.mobizon.kz/service/message/sendsmsmessage?recipient=" + data + "&text=Код валидации : " + code + "&apiKey=kz0502f56621750a9ca3ac636e8301e235c2b647839531f2994222514c786fb6ff2178")
+            await fetch("https://api.mobizon.kz/service/message/sendsmsmessage?recipient=" + data + "&text=Код валидации : " + code + "&apiKey=kz0502f56621750a9ca3ac636e8301e235c2b647839531f2994222514c786fb6ff2178")
             setOpenCerf(true)
           }
         })
